33f88f42233a59b0c90d95f9a18b2409b2ac0ca9
Priyanshi-Bairagi <priyanshibairagi@gmail.com>
2020-05-21 19:27:11 +0530

    Conditional Statement(if else if)

---
+++ Test8.java
@@ -0,0 +1,27 @@
+/*author: priyanshi-bairagi
+  date: 21-05-2020
+  
+ Conditional Statements
+ (if-else if-else)
+ Comparing given number with 15.  */
+ 
+ 
+ class Test8
+ {
+	public static void main(String args[])
+	{
+		int a=10;
+		if(a>15)
+		{
+			System.out.println("Greater");
+		}
+		else if(a<15)
+		{
+			System.out.println("Smaller");
+		}
+		else
+		{
+			System.out.println("Equal");
+		}
+	}
+ }
