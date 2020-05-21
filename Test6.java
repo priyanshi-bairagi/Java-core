eee4d685f7b74aa406a6161879e586c065fab249
Priyanshi-Bairagi <priyanshibairagi@gmail.com>
2020-05-21 19:19:08 +0530

    Ternary operator(Greatest in 3 numbers)

---
+++ Test6.java
@@ -0,0 +1,17 @@
+/*author: priyanshi-bairagi
+  date: 21-05-2020
+  
+ Ternary Operator 
+ Ques: WAP to findout greatest value amongst 3 given numbers */
+ 
+ 
+ class Test6
+ {
+	public static void main(String args[])
+	{
+		int a=10,b=20,c=30;
+		a=a>b?a:b;
+		a=a>c?a:c;
+		System.out.println(a);
+	}
+ }
