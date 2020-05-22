257dd2524768d13fb552c06b218021a048227ac9
Priyanshi-Bairagi <priyanshibairagi@gmail.com>
2020-05-22 14:39:08 +0530

    Switch case (for printing word format of given digit)

---
+++ Test12.java
@@ -0,0 +1,32 @@
+/*author: priyanshi-bairagi
+  date: 22-05-2020
+  
+  Switch Case
+  Ques: WAP to print word/character format of given number*/
+ 
+ class Test12
+ {
+	public static void main(String args[])
+	{
+			int a=5;
+			switch(a)
+			{	case 1:
+					System.out.println("One"); 
+					break;
+				case 2:
+					System.out.println("Two");
+					break;
+				case 3:
+					System.out.println("Three");
+					break;
+				case 4:
+					System.out.println("Four");
+					break;
+				case 5:
+					System.out.println("Five");
+					break;
+				default:
+					System.out.println("Invalid Number");
+			}
+	}
+ }
